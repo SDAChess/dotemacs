17b03465a9c043ea652098980d2c7ec5c175b21e
Simon Scatton <simon.scatton@epita.fr>
2021-09-11 21:30:43 +0200

    Added keybinds, project and Magit

--- init.el
+++ init.el
@@ -39,6 +39,36 @@
 
 ;; Key bindings 
 
+(use-package general
+  :config
+  (general-create-definer sda/leader-def
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC"))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
 ;; Escape quit prompts
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -103,6 +133,7 @@
 
 					; Line numbers
 
+
 (column-number-mode)
 (global-display-line-numbers-mode t)
 (dolist (mode '(org-mode-hook
@@ -123,3 +154,40 @@
   :diminish (which-key-mode)
   :config (setq which-key-idle-delay 0.7))
 
+
+;; Configuration of menus
+(sda/leader-def
+  "f"  '(:ignore t :which-key "Files")
+  "fs" 'save-buffer)
+
+;; One keys map.
+
+(sda/leader-def
+  "p" '(projectile-command-map :which-key "Projects")
+  "w"  '(evil-window-map :which-key "Windows")
+  "," '(switch-to-buffer :which-key "Switch buffers in Workspace") ; Need to add workspaces
+  "<" '(switch-to-buffer :which-key "Switch buffers")
+  "SPC" '(projectile-find-file :which-key "Find file in project")
+  "." '(find-file :which-key "Find file"))
+  
+;; Projects
+
+;; Projectile
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :init
+  (when (file-directory-p "~/Workspace")
+    (setq projectile-project-search-path '(("~/Workspace/" . 3))))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+;; Magit
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
